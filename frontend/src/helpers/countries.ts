43e9ad0bf5f2d0a06bb79f565a3fbdb631562897
Matias Campos <matias98cs@gmail.com>
2025-03-13 20:54:50 -0300

    feat: screen de configuraciones maquetada

---
+++ frontend/src/helpers/countries.ts
@@ -0,0 +1,11 @@
+export const countries = [
+  { code: "AR", name: "Argentina" },
+  { code: "BR", name: "Brasil" },
+  { code: "CL", name: "Chile" },
+  { code: "CO", name: "Colombia" },
+  { code: "MX", name: "México" },
+  { code: "PE", name: "Perú" },
+  { code: "UY", name: "Uruguay" },
+  { code: "US", name: "Estados Unidos" },
+  { code: "ES", name: "España" },
+];
