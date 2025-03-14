d95bac3a1a1453631ede1c323d37c149d795d86c
Matias Campos <matias98cs@gmail.com>
2025-03-13 21:14:36 -0300

    lazy loading para los componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import { Suspense, lazy } from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Configurations from "./pages/Configurations";
-import ListPersons from "./pages/ListPersons";
-import LoadPersons from "./pages/LoadPersons";
+import Loading from "./components/Loading";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Configurations = lazy(() => import("./pages/Configurations"));
+const ListPersons = lazy(() => import("./pages/ListPersons"));
+const LoadPersons = lazy(() => import("./pages/LoadPersons"));
+const NotFound = lazy(() => import("./pages/NotFound")); // Nueva página 404
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/configuracion" element={<Configurations />} />
-          <Route path="/lista" element={<ListPersons />} />
-          <Route path="/carga" element={<LoadPersons />} />
-        </Route>
-      </Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/configuracion" element={<Configurations />} />
+            <Route path="/lista" element={<ListPersons />} />
+            <Route path="/carga" element={<LoadPersons />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
