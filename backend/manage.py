b8942b8f5facdf527c8423343fc0ce7c8017dd98
Matias Campos <matias98cs@gmail.com>
2025-04-12 17:41:05 -0300

    deploy v4

--- backend/manage.py
+++ backend/manage.py
@@ -7,6 +7,11 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproyect.settings')
+
+    if not sys.stdin.isatty():
+        __builtins__.input = lambda prompt="": "yes"
+
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
